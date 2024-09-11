f19c8861f04102aa97b6157f9a6d7ec3a91e4bf6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-11 22:07:23 +0500

    6 - Fetching Data Revalidation

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,31 @@
+async function getTickets() {
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            revalidate: 0
+        }
+    });
+    return res.json();
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets();
+
+    return (
+        <>
+            {
+                tickets.map((ticket, index) => (
+                    <div key={index} className="card my-5">
+                        <h3>{ticket.title}</h3>
+                        <p>{ticket.body.slice(0, 200)}...</p>
+                        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority   </div>
+                    </div>
+                ))
+            }
+            {
+                tickets.length === 0 && (
+                    <p className="text-center">There are no open tickets, yay!</p>
+                )
+            }
+        </>
+    )
+}
