18add0fef1202cad4cbf0452f08eb3950479d58d
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-12 22:46:44 +0500

    7 - Dynamic Segments params

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getTickets() {
     const res = await fetch('http://localhost:4000/tickets', {
         next: {
@@ -15,9 +17,11 @@ export default async function TicketList() {
             {
                 tickets.map((ticket, index) => (
                     <div key={index} className="card my-5">
-                        <h3>{ticket.title}</h3>
-                        <p>{ticket.body.slice(0, 200)}...</p>
-                        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority   </div>
+                        <Link href={`/tickets/${ticket.id}`}>
+                            <h3>{ticket.title}</h3>
+                            <p>{ticket.body.slice(0, 200)}...</p>
+                            <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+                        </Link>
                     </div>
                 ))
             }
