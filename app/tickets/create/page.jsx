c232c3ac4bfd208c7fc83bc80a1e598f6fddd4e6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-17 20:03:20 +0500

    11 - Client Form Component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateFrom from "./CreateFrom";
+
+export default function CreateTicket() {
+    return (
+        <main>
+            <h2 className="text-primary text-center">Add a New Ticket</h2>
+            <CreateFrom />
+        </main>
+    )
+}
