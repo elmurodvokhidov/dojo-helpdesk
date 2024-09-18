81bda56e000a7ae00382180c7fb0204f00b4b542
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-18 23:01:15 +0500

    13 - Route Groups

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <main>
+            <h2 className="text-center">Log in</h2>
+        </main>
+    )
+}
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+    return (
+        <main>
+            <h2 className="text-center">Sign up</h2>
+        </main>
+    )
+}
