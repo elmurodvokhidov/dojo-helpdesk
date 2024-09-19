a32df229fcb2fc5e208d3ef049722cbfceed990a
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-19 19:42:07 +0500

    14 - Nested Layouts

---
+++ app/(dashboard)/layout.jsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+export default function DashboardLayout({ children }) {
+    return (
+        <>
+            <Navbar />
+            {children}
+        </>
+    )
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import { Rubik } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={rubik.className}>
-        <Navbar />
         {children}
       </body>
     </html >
