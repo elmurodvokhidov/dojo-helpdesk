9bf792af1f7a53ac35ae1c684cc8868769b2e6cf
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-20 10:16:33 +0500

    16 - Page Metadata

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -2,6 +2,10 @@ import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
 
+export const metadata = {
+    title: "Dojo Helpdesk | Tickets",
+}
+
 export default function Tickets() {
     return (
         <main>
