75e2145e96b7551e1e0b2b984a12cbfeff247dc3
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-21 20:38:18 +0500

    18 - Handling GET Requests

---
+++ app/api/tickets/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const res = await fetch('http://localhost:3000/api/tickets');
+    const tickets = await res.json();
+    return NextResponse.json(tickets, {
+        status: 200
+    });
+}
