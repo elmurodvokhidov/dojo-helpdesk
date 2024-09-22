a720cca5da46cbbef332bf8cc11d4a44e37bdd25
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-22 23:31:56 +0500

    19 - Dynamic vs Static Route Handlers

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const res = await fetch('http://localhost:3000/api/tickets');
     const tickets = await res.json();
