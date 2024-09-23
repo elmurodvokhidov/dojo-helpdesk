93b65bbc149fc591a7deeaa82e7b1a7268de4d73
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-23 23:33:23 +0500

    20 - Handling POST Requests

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -8,4 +8,20 @@ export async function GET() {
     return NextResponse.json(tickets, {
         status: 200
     });
+};
+
+export async function POST(req) {
+    const ticket = await req.json();
+
+    const res = await fetch('http://localhost:4000/tickets', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(ticket)
+    });
+
+    const newTicket = await res.json();
+
+    return NextResponse.json(newTicket, {
+        status: 201
+    });
 }
