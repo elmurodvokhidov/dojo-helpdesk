b59a6916c869298b730af8bc07b465769eb6aae3
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-25 00:49:10 +0500

    21 - Route Handler Params

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-    const res = await fetch('http://localhost:3000/api/tickets');
+    const res = await fetch('http://localhost:4000/tickets');
     const tickets = await res.json();
     return NextResponse.json(tickets, {
         status: 200
