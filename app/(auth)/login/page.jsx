0338338ac2932184630e23ad3c11208ba7a0fd7c
elmurodvokhidov <elmurodvkh@gmail.com>
2024-09-25 21:59:14 +0500

    23 - Making the Auth Forms

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,18 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function Login() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
     return (
         <main>
             <h2 className="text-center">Log in</h2>
+            <AuthForm handleSubmit={handleSubmit} />
         </main>
     )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,18 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function SignUp() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
     return (
         <main>
             <h2 className="text-center">Sign up</h2>
+            <AuthForm handleSubmit={handleSubmit} />
         </main>
     )
 }
