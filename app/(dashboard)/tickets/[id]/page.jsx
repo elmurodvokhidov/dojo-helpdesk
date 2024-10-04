490c4169b12a1d7aea6878cfc1b0a87be31fa729
elmurodvokhidov <elmurodvkh@gmail.com>
2024-10-04 23:00:19 +0500

    34 - Fetching Single Records

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -1,41 +1,33 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { notFound } from "next/navigation";
+import { cookies } from "next/headers";
 
 export const dynamicParams = true;
 
 export async function generateMetadata({ params }) {
-    const id = params.id;
-
-    const res = await fetch(`http://localhost:4000/tickets/${id}`);
-    const ticket = await res.json();
+    const supabase = createServerComponentClient({ cookies });
+    const { data: ticket } = await supabase.from('tickets')
+        .select()
+        .eq('id', params.id)
+        .single()
 
     return {
-        title: `Dojo Helpdesk | ${ticket.title}`
+        title: `Dojo Helpdesk | ${ticket?.title || 'Ticket not found'}`
     }
-}
-
-export async function generateStaticParams() {
-    const res = await fetch('http://localhost:4000/tickets');
-    const tickets = await res.json();
-    return tickets.map(ticket => ({
-        id: ticket.id
-    }))
 }
 
 async function getTicket(id) {
-    // imitate delay
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    const supabase = createServerComponentClient({ cookies });
+    const { data } = await supabase.from('tickets')
+        .select()
+        .eq('id', id)
+        .single()
 
-    const res = await fetch(`http://localhost:4000/tickets/${id}`, {
-        next: {
-            revalidate: 60
-        }
-    });
-
-    if (!res.ok) {
+    if (!data) {
         notFound()
     }
 
-    return res.json();
+    return data;
 }
 
 export default async function TicketDetails({ params }) {
