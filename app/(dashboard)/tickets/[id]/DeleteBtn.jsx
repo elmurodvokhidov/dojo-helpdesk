7601875294260c4385556f6d2f24118ff4581402
unknown <elmurodvkh@gmail.com>
2024-10-07 22:54:24 +0500

    36 - Deleting Ticket Records

--- app/(dashboard)/tickets/[id]/DeleteBtn.jsx
+++ app/(dashboard)/tickets/[id]/DeleteBtn.jsx
@@ -1,14 +1,27 @@
 "use client"
 
+import { useRouter } from "next/navigation";
 import { useState } from "react"
 import { TiDelete } from "react-icons/ti"
 
 export default function DeleteBtn({ id }) {
     const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
 
     const handleClick = async () => {
         setIsLoading(true);
-        console.log(id);
+        const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+            method: 'DELETE'
+        });
+        const json = await res.json();
+        if (json.error) {
+            console.log(json.error);
+            setIsLoading(false);
+        }
+        if (!json.error) {
+            router.refresh();
+            router.push('/tickets');
+        }
     }
 
     return (
--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,26 +1,13 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export const dynamic = 'force-dynamic';
+export async function DELETE(_, { params }) {
+    const id = params.id;
+    const supabase = createRouteHandlerClient({ cookies });
+    const { error } = await supabase.from('tickets')
+        .delete()
+        .eq('id', id)
 
-export async function GET(_, { params }) {
-    const id = params.id
-
-    const res = await fetch(`http://localhost:4000/tickets/${id}`);
-    const ticket = await res.json();
-
-    if (!res.ok) {
-        return NextResponse.json(
-            {
-                error: "Cannot find the ticket"
-            },
-            {
-                status: 404
-
-            }
-        );
-    }
-
-    return NextResponse.json(ticket, {
-        status: 200
-    });
-};
+    return NextResponse.json({ error });
+}
